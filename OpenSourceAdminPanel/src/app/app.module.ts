c9a9863fb613c252828ee47b2fe061f0db202f57
RAJNEESH KHARE <rkhare@gammastack.com>
2022-03-09 00:38:23 +0530

    added commonly shared components and services

--- OpenSourceAdminPanel/src/app/app.module.ts
+++ OpenSourceAdminPanel/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule
